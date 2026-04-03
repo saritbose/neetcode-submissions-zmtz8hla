3288f4fd5b8d4b8294b7e449d7bafc94a64d3f6f
Sarit <saritbose26@gmail.com>
2026-04-03 10:44:25 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++) {
+            if(m.find(nums[i]) == m.end()) {
+                m[nums[i]] = 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
