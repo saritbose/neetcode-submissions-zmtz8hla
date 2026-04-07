c71b61ceb57bdaed37c464ed6f57ea635ded993a
Sarit <saritbose26@gmail.com>
2026-04-07 17:51:28 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+public:
+    stack<pair<int,int>> st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()) {
+            st.push({val,val});
+        } else {
+            int minNum = min(val,st.top().second);
+            st.push({val,minNum});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
