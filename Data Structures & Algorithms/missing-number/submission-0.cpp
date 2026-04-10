ee58514e36a4cef56179ebca54ccd6c7c68b951e
Sarit <saritbose26@gmail.com>
2026-04-10 17:58:52 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i=1;i<=nums.size();i++) {
+            ans = ans ^ i;
+        }
+        for(int i : nums) {
+            ans = ans ^ i;
+        }
+        return ans;
+    }
+};
