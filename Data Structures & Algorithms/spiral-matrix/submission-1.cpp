10fc3e2a7c9bc69dbb148002c2a80141b79f417c
Sarit <saritbose26@gmail.com>
2026-04-17 09:30:45 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size(); // row
+        int m = matrix[0].size(); // column
+        vector<int> ans;
+        int r1 = 0, r2 = n - 1, c1 = 0, c2 = m - 1;
+
+        while(r1 <= r2 && c1 <= c2) {
+            for(int i=c1;i<=c2;i++) {
+                ans.push_back(matrix[r1][i]);
+            }
+            for(int i=r1+1;i<=r2;i++) {
+                ans.push_back(matrix[i][c2]);
+            }
+            for(int i=c2-1;i>=c1;i--) {
+                if(r1 == r2) break;
+                ans.push_back(matrix[r2][i]);
+            }
+            for(int i=r2-1;i>r1;i--) {
+                if(c1 == c2) break;
+                ans.push_back(matrix[i][c1]);
+            }
+            r1++;c1++;r2--;c2--;
+        }
+        return ans;
+    }
+};
